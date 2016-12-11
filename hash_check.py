3958dcb8d4dcb56b77aefda2012529ee896d5543
Alex Rocha <alex.rochas@yahoo.com.br>
2016-12-11 11:23:38 -0200

    feat: script draft

---
+++ hash_check.py
@@ -0,0 +1,30 @@
+import getopt
+import zipfile
+
+import sys
+
+
+def show_jar_classes(jar_file):
+    zf = zipfile.ZipFile(jar_file, 'r')
+    try:
+        lst = zf.infolist()
+        for zi in lst:
+            fn = zi.filename
+            if fn == 'git.properties':
+                    print(zf.read(fn).decode('utf-8'))
+    finally:
+        zf.close()
+
+if __name__ == '__main__':
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "ha:", ["artifact="])
+    except getopt.GetoptError:
+        print('hash_check.py -a <artifact_path>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('test.py -a <artifact_path>')
+            sys.exit()
+        elif opt in ("-a", "--artifact"):
+            artifact = arg
+            show_jar_classes(artifact)
